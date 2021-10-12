f5c1b7306a268ad581d869132e64db95f4179498
Mateo Estrada <mateoej12@gmail.com>
2021-10-12 10:21:20 -0700

    Added the tables.

--- distance.js
+++ distance.js
@@ -49,45 +49,145 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 }
 
 
+
+
+// The origin that will be used to make the calculation of the distance. 
+const osu = {
+	"osu":"Corvallis",
+	"lat":"44.5646",
+	"lng":"-123.2620"
+}
+
+
+fetch("cities.json")
+    .then(response => response.json())
+    .then(data => generateTableMi(data, osu))
+    .then(data => generateTableKm(data, osu))
+
+function calculateDistance(data, osu, unit){
+    // Calculates the distance in Miles
+    // Returns an array of distances
+    var arrayDistances = [];
+    const osuLat = osu['lat'];
+    const osuLng = osu['lng'];
+    for (let i = 0; i < data.length; i++){
+        const dist = distance(osuLat, osuLng, data[i]['lat'], data[i]['lng'], unit);
+        arrayDistances.push(dist);
+    }
+    return arrayDistances;
+}
+
+
 /**
  * The parameters are: lat1, lon1, lat2, lon2, unit (M:miles, K:km, N:nautical miles)
  * I will assume point 1 is Corvallis and point 2 is the other city that I will be using to calculate the distance from.
  * Example calculations between osu and cities:
  * Will use parseFloat() function to change the strings from the json objects to floats.
  */
-
-function generate_table1(){
-    // 1
-    // const latOsu = origin['lat'];
-    // const lngOsu = origin['lng'];
-    // // 2
-    // const latSeattle = cities[0]['lat'];
-    // const lngSeattle = cities[0]['lng'];
-    // const distance_to_seattle = distance(latOsu, lngOsu, latSeattle, lngSeattle, "M");
-    // console.log(distance_to_seattle);
-
-
-
+function generateTableMi(data, osu){
+    // Makes the table using DOM
+    var arrDistances = calculateDistance(data, osu, "M");
+    
     // Grabs the body tag from the html page to add a table to it
     var body = document.getElementsByTagName("body")[0];
 
     // Make the table
     var tbl = document.createElement("table");
+    tbl.setAttribute('class', 'tblMi');
     // Make the table body
     var tblBody = document.createElement("tbody");
-
-    // make the cells of the table
-    for (let i = 0; i < 3; i++){
+    // Make sure the table has the descriptions.
+    const titles = ["Names:", "Latitude:", "Longitude", "Distance (Mil)"];
+    var row = document.createElement("tr");
+    for (let k = 0; k < 4; k++){
+        var th = document.createElement("th");
+        var thText =document.createTextNode(titles[k]);
+        th.appendChild(thText);
+        row.appendChild(th);
+        tblBody.appendChild(row);
+        tbl.appendChild(tblBody);
+    }
+    for (let i = 0; i < data.length; i++){
         // make a table row
         var row = document.createElement("tr");
-        
-        for (let j = 0; j < 3; j++){
+        for (let j = 0; j < titles.length; j++){
             // make a table data and its text content and add it to the table row.
             var cell = document.createElement("td");
-            var cellText = document.createTextNode("Cell in row "+ i +", column" +j);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        }
+            if (j === 0){
+                var cellText = document.createTextNode(data[i]['city']);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (j == 1){
+                var cellText = document.createTextNode(data[i]['lat']);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (j === 2){
+                var cellText = document.createTextNode(data[i]['lng']);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (j === 3){
+                var cellText = document.createTextNode(`${arrDistances[i]}`);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+        }     
+        // Add the row to the end of the table body
+        tblBody.appendChild(row);
+    }
+    // Put the table body into the table
+    tbl.appendChild(tblBody);
+    tbl.appendChild(document.createElement('br'));
+    body.appendChild(tbl);
+    return data;
+}
+
+
+function generateTableKm(data, osu){
+    // Makes the table using DOM
+    var arrDistances = calculateDistance(data, osu, "K");
+    // Grabs the body tag from the html page to add a table to it
+    var body = document.getElementsByTagName("body")[0];
+    // Make the table with class
+    var tbl = document.createElement("table");
+    tbl.setAttribute('class', 'tblKm');
+    // Make the table body
+    var tblBody = document.createElement("tbody");
+    // Make sure the table has the descriptions.
+    const titles = ["Names:", "Latitude:", "Longitude", "Distance (Km)"];
+    var row = document.createElement("tr");
+    for (let k = 0; k < 4; k++){
+        var th = document.createElement("th");
+        var thText =document.createTextNode(titles[k]);
+        th.appendChild(thText);
+        row.appendChild(th);
+        tblBody.appendChild(row);
+        tbl.appendChild(tblBody);
+    }
+    for (let i = 0; i < data.length; i++){
+        // make a table row
+        var row = document.createElement("tr");
+        for (let j = 0; j < titles.length; j++){
+            // make a table data and its text content and add it to the table row.
+            var cell = document.createElement("td");
+            if (j === 0){
+                var cellText = document.createTextNode(data[i]['city']);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (j == 1){
+                var cellText = document.createTextNode(data[i]['lat']);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+
+            } else if (j === 2){
+                var cellText = document.createTextNode(data[i]['lng']);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (j === 3){
+                var cellText = document.createTextNode(`${arrDistances[i]}`);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+        }     
         // Add the row to the end of the table body
         tblBody.appendChild(row);
 
@@ -97,46 +197,11 @@ function generate_table1(){
     body.appendChild(tbl);
 }
 
-
-
-function generate_table2(){
-    // 1
-    // const latOsu = origin['lat'];
-    // const lngOsu = origin['lng'];
-    // // 2
-    // const latSeattle = cities[0]['lat'];
-    // const lngSeattle = cities[0]['lng'];
-    // const distance_to_seattle = distance(latOsu, lngOsu, latSeattle, lngSeattle, "M");
-    // console.log(distance_to_seattle);
-
-
-
-    // Grabs the body tag from the html page to add a table to it
-    var body = document.getElementsByTagName("body")[0];
-
-    // Make the table
-    var tbl = document.createElement("table");
-    // Make the table body
-    var tblBody = document.createElement("tbody");
-
-    // make the cells of the table
-    for (let i = 0; i < 3; i++){
-        // make a table row
-        var row = document.createElement("tr");
-        
-        for (let j = 0; j < 3; j++){
-            // make a table data and its text content and add it to the table row.
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode("Cell in row "+ i +", column" +j);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        }
-        // Add the row to the end of the table body
-        tblBody.appendChild(row);
-
+function sortMilesTable(){
+    var body = document.getElementsByTagName('body')[0];
+    var tbl = document.getElementsByTagName('table')[0];
+    const r = tbl.rows;
+    for (let i = 1; i < r.length - 1; i++){
+        console.log(r[i]);
     }
-    // Put the table body into the table
-    tbl.appendChild(tblBody);
-    body.appendChild(tbl);
 }
-
